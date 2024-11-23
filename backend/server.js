84a7cfec2b482c604eea8cb950c36942aa36a0fb
vanessalayson <vanessalayson25@gmail.com>
2024-11-23 14:26:16 +0800

    Third commit. Added UPDATE route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -36,6 +37,24 @@ app.post("/api/products", async (req, res) => {
         res.status(201).json({ success: true, data: newProduct });
     } catch (error) {
         console.error("Error in Create product", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
+// UPDATE ROUTE
+app.put("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({ success: false, message: "Invalid Product Id" });
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
         res.status(500).json({ success: false, message: "Server Error" });
     }
 });
